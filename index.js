c883d2beb5463e94b78b3bf2a3e8111a150bde1a
Cully Larson <cully.larson@gmai.com>
2015-10-18 21:45:37 -0700

    Added the ability for subcommands, and moved all the sync stuff into a subcommand.

--- index.js
+++ index.js
@@ -1,141 +1,21 @@
 #!/usr/bin/env node --harmony
 "use strict"
 
-// TODO -- handle promise rejection
-
 let commandLineArgs = require("command-line-args")
 let error = require("./lib/error")
-let datle = require("./lib/datle")
 let basename = require("basename")
-let jsonfile = require("jsonfile")
-let Promise = require("promise")
-let actionUtil = require("./lib/action-util")
-let harvestUtil = require("./lib/harvest-util")
-let actionPrinter = require("./lib/action-printer")
-let confirmActions = require("./lib/confirm-actions")
-let chalk = require("chalk")
+
+let subcommandSync = require("./lib/subcommands/sync")
 
 {
     let args = getArguments()
-    let config = readConfig(args.config)
-    let harvestSource = harvestUtil.getHarvestSource(config)
-    let harvestDest = harvestUtil.getHarvestDest(config)
 
-    actionUtil.getSyncActions(harvestSource, harvestDest, datle(config.start), config.sync)
-        .then((actions) => {
-            actionPrinter.summary(actions)
-
-            // no actions to perform
-            if(!actions.missing.length) {
-                console.log(chalk.bold.cyan("Everything is already synced. No actions to perform.\n"))
-                process.exit(0)
-            }
-            // confirm performance of actions
-            else {
-                return confirmActions(actions)
-            }
-        })
-        .then((actions) => {
-            return actionUtil.performSyncActions(harvestDest, actions.missing)
-        }, () => {
-            console.log(chalk.bold.cyan("Exiting without performing sync.\n"))
-            process.exit(0)
-        })
-        .then((results) => {
-            // no actions performed
-            if(!results.success.length && !results.failure.length) {
-                console.log(chalk.bold.yellow("No changes were made.\n"))
-                process.exit(0)
-            }
-            // all successful
-            else if(!results.failure.length) {
-                console.log(chalk.bold.green("All times were sent to the destination successfully.\n"))
-                process.exit(0)
-            }
-            // else, some failure
-            else {
-                actionPrinter.failure({"missing": results.failure, "present": []})
-            }
-        })
+    if(args.subcommand === 'sync') subcommandSync(args.config)
 }
 
 /*
  * Functions
  */
-
-function analyzeConfig(config) {
-    if(!config.source) return "Your config must define: source"
-    if(!config.dest) return "Your config must define: dest"
-    if(!config.source.subdomain) return "Your config must define: source.subdomain"
-    if(!config.source.email) return "Your config must define: source.email"
-    if(!config.source.password) return "Your config must define: source.password"
-    if(!config.dest.subdomain) return "Your config must define: dest.subdomain"
-    if(!config.dest.email) return "Your config must define: dest.email"
-    if(!config.dest.password) return "Your config must define: dest.password"
-    if(!config.start) return "Your config must define: start"
-    if(!config.sync) return "Your config must define: sync"
-    if(!Object.keys(config.sync).length) return "Your config must define at least one sync item"
-
-    let startProblem = analyzeStart(config.start)
-    if(startProblem) return startProblem
-
-    let syncProblem = analyzeSync(config.sync)
-    if(syncProblem) return syncProblem
-}
-
-function analyzeStart(start) {
-    if(!start.match(/^(\d{4})-(\d{2})-(\d{2})$/)) return "'start' must be in the format: YYYY-MM-DD"
-
-    if(isNaN(Date.parse(start))) return "The start date provided is not a valid date"
-}
-
-function analyzeSync(sync) {
-    let analyzeParts = function(type, str, parts) {
-        if(!parts.client) return type + " sync string [" + str + "] is missing client, project, and task"
-        if(!parts.project) return type + " sync string [" + str + "] is missing project and task"
-        if(!parts.task) return type + " sync string [" + str + "] is missing task"
-        if(parts.overflow) return type + " sync string [" + str + "] must only contain a client, project, and task. Other pieces were found."
-    }
-
-    for(let key in sync) {
-        if(!sync.hasOwnProperty(key)) continue
-
-        let source = key
-        let dest = sync[key]
-
-        let sourceParts = actionUtil.parseSyncItem(source)
-        let destParts = actionUtil.parseSyncItem(dest)
-
-        let sourceProblem = analyzeParts("Source", source, sourceParts)
-        if(sourceProblem) return sourceProblem
-
-        let destProblem = analyzeParts("Destination", dest, destParts)
-        if(destProblem) return destProblem
-    }
-}
-
-function verifyConfig(config) {
-    let configProblem = analyzeConfig(config)
-    if(configProblem) {
-        error(configProblem)
-        process.exit(3)
-    }
-}
-
-function readConfig(configFilename) {
-    let config
-    try {
-        config = jsonfile.readFileSync(configFilename)
-    }
-    catch(ex) {
-        error(ex.message)
-        process.exit(2)
-    }
-
-    verifyConfig(config)
-
-    return config
-}
 
 function verifyArguments(args, cliUsage) {
     // just want help
@@ -145,7 +25,7 @@ function verifyArguments(args, cliUsage) {
     }
 
     // no config file
-    if(!args.config) {
+    if(args.subcommand === 'sync' && !args.config) {
         console.log(cliUsage)
         process.exit(1)
     }
@@ -156,7 +36,7 @@ function getArguments() {
 
     let cli = commandLineArgs(
         [
-            { name: "config", type: String, multiple: false, defaultOption: true },
+            { name: "bare", type: String, multiple: true, defaultOption: true },
             { name: "help", alias: "h", type: Boolean, defaultValue: false },
         ]
     )
@@ -171,9 +51,49 @@ function getArguments() {
         ],
     })
 
-    let args = cli.parse()
+    let args = parseBareParams(cli.parse(), cliUsage)
 
     verifyArguments(args, cliUsage)
+
+    return args
+}
+
+function parseBareParams(args, cliUsage) {
+    let validSubCommands = [
+        "sync",
+        "config",
+    ]
+
+    let defaultSubCommand = 'sync'
+
+    // parse the bare params
+    if(args.bare) {
+        // more than one
+        if(args.bare.length) {
+            // it's a sub-command
+            if(validSubCommands.indexOf(args.bare[0]) !== -1) {
+                args.subcommand = args.bare[0]
+
+                // if there's another argument, assume it's the config
+                if(args.bare.length > 1) {
+                    args.config = args.bare[1]
+                }
+            }
+            // not a sub-command
+            else {
+                // if there's more than one
+                if(args.bare.length > 1) {
+                    console.log(cliUsage)
+                    process.exit(20)
+                }
+                // just one
+                else {
+                    args.subcommand = defaultSubCommand
+                    args.config = args.bare[0]
+                }
+            }
+        }
+    }
 
     return args
 }
